60bbb8a6b515828e373c7fa23e3fb792dc55d359
Tirinox <developer@tirinox.ru>
2023-10-05 14:02:26 +0600

    Clean old code from main

--- src/main_lang_bot.py
+++ src/main_lang_bot.py
@@ -4,15 +4,12 @@ import os
 from aiogram import Bot, Dispatcher, executor
 from dotenv import load_dotenv
 
-from dialogue import register_dialogs
-from manager import Manager
+from logs import setup_logs
 
 
 def main():
     load_dotenv()
-
-    # Configure logging
-    logging.basicConfig(level=logging.INFO)
+    setup_logs(log_level=logging.INFO)
 
     # Read API token from environment and create bot
     api_token = os.getenv("TG_TOKEN")
@@ -25,11 +22,14 @@ def main():
     bot = Bot(token=api_token, proxy=proxy)
     dp = Dispatcher(bot)
 
-    manager = Manager('sessions.json')
-    register_dialogs(dp, manager)
+    # manager = Manager('sessions.json')
+    # register_dialogs(dp, manager)
 
     # Start the Bot
-    executor.start_polling(dp, skip_updates=True, on_startup=manager.run_in_background)
+    executor.start_polling(
+        dp, skip_updates=True,
+        # on_startup=manager.run_in_background
+    )
 
 
 if __name__ == '__main__':
